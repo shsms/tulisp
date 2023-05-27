d8df14ff5c82a9167c7bb0b79f00ee72acf5ab91
Sahas Subramanian <sahas.subramanian@proton.me>
2023-05-27 14:52:52 +0200

    Move existing sequence functions to `sequences.rs`

--- src/builtin/functions/functions.rs
+++ src/builtin/functions/functions.rs
@@ -10,7 +10,6 @@ use crate::lists;
 use crate::TulispObject;
 use crate::TulispValue;
 use crate::{destruct_bind, list};
-use std::cmp::Ordering;
 use std::convert::TryInto;
 use std::rc::Rc;
 use tulisp_proc_macros::{crate_fn, crate_fn_no_eval};
@@ -634,11 +633,6 @@ pub(crate) fn add(ctx: &mut TulispContext) {
     }
 
     #[crate_fn(add_func = "ctx")]
-    fn length(list: TulispObject) -> Result<i64, Error> {
-        lists::length(&list)
-    }
-
-    #[crate_fn(add_func = "ctx")]
     fn nthcdr(n: i64, list: TulispObject) -> Result<TulispObject, Error> {
         lists::nthcdr(n, list)
     }
@@ -659,30 +653,6 @@ pub(crate) fn add(ctx: &mut TulispContext) {
             ret.push(ctx.eval(&list!(,func.clone() ,quote(item))?)?)?;
         }
         Ok(ret.into())
-    }
-
-    #[crate_fn(add_func = "ctx")]
-    fn sort(
-        ctx: &mut TulispContext,
-        seq: TulispObject,
-        pred: TulispObject,
-    ) -> Result<TulispObject, Error> {
-        let pred = eval(ctx, &pred)?;
-        let mut vec: Vec<_> = seq.base_iter().collect();
-        vec.sort_by(|v1, v2| {
-            let vv = list!(,TulispObject::nil() ,v1.clone() ,v2.clone()).unwrap();
-            vv.with_ctxobj(Some(pred.clone()));
-
-            if eval_check_null(ctx, &vv).unwrap_or_else(|_| false) {
-                Ordering::Equal
-            } else {
-                Ordering::Less
-            }
-        });
-        let ret = vec
-            .iter()
-            .fold(list!(), |v1, v2| list!(,@v1 ,(*v2).clone()).unwrap());
-        Ok(ret)
     }
 
     #[crate_fn(add_func = "ctx")]
---
+++ src/builtin/functions/sequences.rs
@@ -0,0 +1,37 @@
+use crate::{
+    eval::{eval, eval_check_null},
+    list, lists, Error, TulispContext, TulispObject,
+};
+use std::cmp::Ordering;
+use tulisp_proc_macros::crate_fn;
+
+pub(crate) fn add(ctx: &mut TulispContext) {
+    #[crate_fn(add_func = "ctx")]
+    fn length(list: TulispObject) -> Result<i64, Error> {
+        lists::length(&list)
+    }
+
+    #[crate_fn(add_func = "ctx")]
+    fn sort(
+        ctx: &mut TulispContext,
+        seq: TulispObject,
+        pred: TulispObject,
+    ) -> Result<TulispObject, Error> {
+        let pred = eval(ctx, &pred)?;
+        let mut vec: Vec<_> = seq.base_iter().collect();
+        vec.sort_by(|v1, v2| {
+            let vv = list!(,TulispObject::nil() ,v1.clone() ,v2.clone()).unwrap();
+            vv.with_ctxobj(Some(pred.clone()));
+
+            if eval_check_null(ctx, &vv).unwrap_or_else(|_| false) {
+                Ordering::Equal
+            } else {
+                Ordering::Less
+            }
+        });
+        let ret = vec
+            .iter()
+            .fold(list!(), |v1, v2| list!(,@v1 ,(*v2).clone()).unwrap());
+        Ok(ret)
+    }
+}
