e76c1fbdb8b8e1d0ca3757e1a0f7ec373401ac6a
Sahas Subramanian <sahas.subramanian@proton.me>
2023-08-12 18:51:10 +0200

    Move functions for accessing list elements into a separate submodule

--- src/builtin/functions/functions.rs
+++ src/builtin/functions/functions.rs
@@ -531,36 +531,6 @@ pub(crate) fn add(ctx: &mut TulispContext) {
 
     // List functions
 
-    #[crate_fn(add_func = "ctx", name = "car")]
-    fn impl_car(name: TulispObject) -> Result<TulispObject, Error> {
-        name.car()
-    }
-
-    #[crate_fn(add_func = "ctx", name = "cdr")]
-    fn impl_cdr(name: TulispObject) -> Result<TulispObject, Error> {
-        name.cdr()
-    }
-
-    #[crate_fn(add_func = "ctx", name = "caar")]
-    fn impl_caar(name: TulispObject) -> Result<TulispObject, Error> {
-        name.caar()
-    }
-
-    #[crate_fn(add_func = "ctx", name = "cadr")]
-    fn impl_cadr(name: TulispObject) -> Result<TulispObject, Error> {
-        name.cadr()
-    }
-
-    #[crate_fn(add_func = "ctx", name = "cdar")]
-    fn impl_cdar(name: TulispObject) -> Result<TulispObject, Error> {
-        name.cdar()
-    }
-
-    #[crate_fn(add_func = "ctx", name = "cddr")]
-    fn impl_cddr(name: TulispObject) -> Result<TulispObject, Error> {
-        name.cddr()
-    }
-
     #[crate_fn(add_func = "ctx", name = "cons")]
     fn impl_cons(car: TulispObject, cdr: TulispObject) -> TulispObject {
         TulispObject::cons(car, cdr)
@@ -629,16 +599,6 @@ pub(crate) fn add(ctx: &mut TulispContext) {
                 .with_span(span)),
             None => Ok(TulispObject::nil()),
         }
-    }
-
-    #[crate_fn(add_func = "ctx")]
-    fn nthcdr(n: i64, list: TulispObject) -> Result<TulispObject, Error> {
-        lists::nthcdr(n, list)
-    }
-
-    #[crate_fn(add_func = "ctx")]
-    fn nth(n: i64, list: TulispObject) -> Result<TulispObject, Error> {
-        lists::nth(n, list)
     }
 
     #[crate_fn(add_func = "ctx")]
---
+++ src/builtin/functions/list_elements.rs
@@ -0,0 +1,45 @@
+use tulisp_proc_macros::crate_fn;
+
+use crate::{lists, Error, TulispContext, TulispObject};
+
+pub(crate) fn add(ctx: &mut TulispContext) {
+    #[crate_fn(add_func = "ctx", name = "car")]
+    fn impl_car(name: TulispObject) -> Result<TulispObject, Error> {
+        name.car()
+    }
+
+    #[crate_fn(add_func = "ctx", name = "cdr")]
+    fn impl_cdr(name: TulispObject) -> Result<TulispObject, Error> {
+        name.cdr()
+    }
+
+    #[crate_fn(add_func = "ctx", name = "caar")]
+    fn impl_caar(name: TulispObject) -> Result<TulispObject, Error> {
+        name.caar()
+    }
+
+    #[crate_fn(add_func = "ctx", name = "cadr")]
+    fn impl_cadr(name: TulispObject) -> Result<TulispObject, Error> {
+        name.cadr()
+    }
+
+    #[crate_fn(add_func = "ctx", name = "cdar")]
+    fn impl_cdar(name: TulispObject) -> Result<TulispObject, Error> {
+        name.cdar()
+    }
+
+    #[crate_fn(add_func = "ctx", name = "cddr")]
+    fn impl_cddr(name: TulispObject) -> Result<TulispObject, Error> {
+        name.cddr()
+    }
+
+    #[crate_fn(add_func = "ctx")]
+    fn nth(n: i64, list: TulispObject) -> Result<TulispObject, Error> {
+        lists::nth(n, list)
+    }
+
+    #[crate_fn(add_func = "ctx")]
+    fn nthcdr(n: i64, list: TulispObject) -> Result<TulispObject, Error> {
+        lists::nthcdr(n, list)
+    }
+}
