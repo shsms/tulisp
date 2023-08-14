6cd7dc91deb2b42ca278ac8ce29fe68b9c2ccc6b
Sahas Subramanian <sahas.subramanian@proton.me>
2023-08-14 16:47:29 +0200

    Move shared macros to top-level `builtin::functions` module

--- src/builtin/functions/functions.rs
+++ src/builtin/functions/functions.rs
@@ -16,46 +16,6 @@ use std::convert::TryInto;
 use std::rc::Rc;
 use tulisp_proc_macros::{crate_fn, crate_fn_no_eval};
 
-macro_rules! max_min_ops {
-    ($oper:tt) => {{
-        |selfobj: &TulispObject, other: &TulispObject| -> Result<TulispObject, Error> {
-            if selfobj.floatp() {
-                let s: f64 = selfobj.as_float().unwrap();
-                let o: f64 = other.try_into()?;
-                Ok(f64::$oper(s, o).into())
-            } else if other.floatp() {
-                let o: f64 = other.as_float().unwrap();
-                let s: f64 = selfobj.try_into()?;
-                Ok(f64::$oper(s, o).into())
-            } else {
-                let s: i64 = selfobj.try_into()?;
-                let o: i64 = other.try_into()?;
-                Ok(std::cmp::$oper(s, o).into())
-            }
-        }
-    }};
-}
-
-macro_rules! binary_ops {
-    ($oper:expr) => {{
-        |selfobj: &TulispObject, other: &TulispObject| -> Result<TulispObject, Error> {
-            if selfobj.floatp() {
-                let s: f64 = selfobj.as_float().unwrap();
-                let o: f64 = other.try_into()?;
-                Ok($oper(&s, &o).into())
-            } else if other.floatp() {
-                let o: f64 = other.as_float().unwrap();
-                let s: f64 = selfobj.try_into()?;
-                Ok($oper(&s, &o).into())
-            } else {
-                let s: i64 = selfobj.try_into()?;
-                let o: i64 = other.try_into()?;
-                Ok($oper(&s, &o).into())
-            }
-        }
-    }};
-}
-
 fn reduce_with(
     ctx: &mut TulispContext,
     list: &TulispObject,
@@ -142,14 +102,6 @@ fn mark_tail_calls(
     };
     ret.push(new_tail.with_ctxobj(ctxobj).with_span(span))?;
     Ok(ret)
-}
-
-macro_rules! intern_set_func {
-    ($ctx:ident, $func: ident, $name: literal) => {
-        $ctx.intern($name)
-            .set_scope(TulispValue::Func(Rc::new($func)).into())
-            .unwrap();
-    };
 }
 
 pub(crate) fn add(ctx: &mut TulispContext) {
--- src/builtin/functions/mod.rs
+++ src/builtin/functions/mod.rs
@@ -1,5 +1,55 @@
 use crate::TulispContext;
 
+// These macros are used by multiple submodules, so they are defined here.
+
+macro_rules! max_min_ops {
+    ($oper:tt) => {{
+        |selfobj: &TulispObject, other: &TulispObject| -> Result<TulispObject, Error> {
+            if selfobj.floatp() {
+                let s: f64 = selfobj.as_float().unwrap();
+                let o: f64 = other.try_into()?;
+                Ok(f64::$oper(s, o).into())
+            } else if other.floatp() {
+                let o: f64 = other.as_float().unwrap();
+                let s: f64 = selfobj.try_into()?;
+                Ok(f64::$oper(s, o).into())
+            } else {
+                let s: i64 = selfobj.try_into()?;
+                let o: i64 = other.try_into()?;
+                Ok(std::cmp::$oper(s, o).into())
+            }
+        }
+    }};
+}
+
+macro_rules! binary_ops {
+    ($oper:expr) => {{
+        |selfobj: &TulispObject, other: &TulispObject| -> Result<TulispObject, Error> {
+            if selfobj.floatp() {
+                let s: f64 = selfobj.as_float().unwrap();
+                let o: f64 = other.try_into()?;
+                Ok($oper(&s, &o).into())
+            } else if other.floatp() {
+                let o: f64 = other.as_float().unwrap();
+                let s: f64 = selfobj.try_into()?;
+                Ok($oper(&s, &o).into())
+            } else {
+                let s: i64 = selfobj.try_into()?;
+                let o: i64 = other.try_into()?;
+                Ok($oper(&s, &o).into())
+            }
+        }
+    }};
+}
+
+macro_rules! intern_set_func {
+    ($ctx:ident, $func: ident, $name: literal) => {
+        $ctx.intern($name)
+            .set_scope(TulispValue::Func(Rc::new($func)).into())
+            .unwrap();
+    };
+}
+
 mod conditionals;
 mod functions;
 mod list_elements;
