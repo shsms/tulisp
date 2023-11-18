1df88095793095228bf4ea6b6ff2bd393666a3e3
Sahas Subramanian <sahas.subramanian@proton.me>
2023-11-18 14:34:21 +0100

    Expose `intern` through lisp
    Earlier it was available only through rust.

--- src/builtin/functions/functions.rs
+++ src/builtin/functions/functions.rs
@@ -98,6 +98,11 @@ pub(crate) fn add(ctx: &mut TulispContext) {
     }
 
     #[crate_fn(add_func = "ctx")]
+    fn intern(ctx: &mut TulispContext, name: String) -> Result<TulispObject, Error> {
+        Ok(ctx.intern(&name))
+    }
+
+    #[crate_fn(add_func = "ctx")]
     fn expt(base: TulispObject, pow: TulispObject) -> Result<TulispObject, Error> {
         Ok(f64::powf(base.try_into()?, pow.try_into()?).into())
     }
