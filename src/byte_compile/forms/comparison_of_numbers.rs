5dc2664ef03d377e808c04f5c095ee637a565d67
Sahas Subramanian <sahas.subramanian@proton.me>
2023-12-31 23:48:53 +0100

    Update comparison functions to accept arbitrary number of arguments

--- src/byte_compile/forms/comparison_of_numbers.rs
+++ src/byte_compile/forms/comparison_of_numbers.rs
@@ -1,20 +1,46 @@
-use crate::{byte_compile::Compiler, vm::Instruction, Error, TulispObject};
+use crate::{
+    byte_compile::Compiler,
+    vm::{Instruction, Pos},
+    Error, TulispObject,
+};
+
+fn compile_fn_compare(
+    compiler: &mut Compiler<'_>,
+    name: &TulispObject,
+    args: &TulispObject,
+    instruction: Instruction,
+) -> Result<Vec<Instruction>, Error> {
+    if !compiler.keep_result {
+        return Ok(vec![]);
+    }
+    let label = TulispObject::symbol("_".to_string(), false);
+    let mut result = vec![];
+    let args = args.base_iter().collect::<Vec<_>>();
+    if args.len() < 2 {
+        return Err(Error::new(
+            crate::ErrorKind::ArityMismatch,
+            format!("{} requires at least 2 arguments.", name),
+        ));
+    }
+    for items in args.windows(2) {
+        result.append(&mut compiler.compile_expr(&items[1])?);
+        result.append(&mut compiler.compile_expr(&items[0])?);
+        result.push(instruction.clone());
+        result.push(Instruction::JumpIfNilElsePop(Pos::Label(label.clone())));
+    }
+    result.pop();
+    if args.len() > 2 {
+        result.push(Instruction::Label(label));
+    }
+    Ok(result)
+}
 
 pub(super) fn compile_fn_lt(
     compiler: &mut Compiler<'_>,
     name: &TulispObject,
     args: &TulispObject,
 ) -> Result<Vec<Instruction>, Error> {
-    compiler.compile_2_arg_call(name, args, false, |compiler, arg1, arg2, _| {
-        if !compiler.keep_result {
-            Ok(vec![])
-        } else {
-            let mut result = compiler.compile_expr(arg2)?;
-            result.append(&mut compiler.compile_expr(arg1)?);
-            result.push(Instruction::Lt);
-            Ok(result)
-        }
-    })
+    compile_fn_compare(compiler, name, args, Instruction::Lt)
 }
 
 pub(super) fn compile_fn_le(
@@ -22,16 +48,7 @@ pub(super) fn compile_fn_le(
     name: &TulispObject,
     args: &TulispObject,
 ) -> Result<Vec<Instruction>, Error> {
-    compiler.compile_2_arg_call(name, args, false, |compiler, arg1, arg2, _| {
-        if !compiler.keep_result {
-            Ok(vec![])
-        } else {
-            let mut result = compiler.compile_expr(arg2)?;
-            result.append(&mut compiler.compile_expr(arg1)?);
-            result.push(Instruction::LtEq);
-            Ok(result)
-        }
-    })
+    compile_fn_compare(compiler, name, args, Instruction::LtEq)
 }
 
 pub(super) fn compile_fn_gt(
@@ -39,16 +56,7 @@ pub(super) fn compile_fn_gt(
     name: &TulispObject,
     args: &TulispObject,
 ) -> Result<Vec<Instruction>, Error> {
-    compiler.compile_2_arg_call(name, args, false, |compiler, arg1, arg2, _| {
-        if !compiler.keep_result {
-            Ok(vec![])
-        } else {
-            let mut result = compiler.compile_expr(arg2)?;
-            result.append(&mut compiler.compile_expr(arg1)?);
-            result.push(Instruction::Gt);
-            Ok(result)
-        }
-    })
+    compile_fn_compare(compiler, name, args, Instruction::Gt)
 }
 
 pub(super) fn compile_fn_ge(
@@ -56,16 +64,7 @@ pub(super) fn compile_fn_ge(
     name: &TulispObject,
     args: &TulispObject,
 ) -> Result<Vec<Instruction>, Error> {
-    compiler.compile_2_arg_call(name, args, false, |compiler, arg1, arg2, _| {
-        if !compiler.keep_result {
-            Ok(vec![])
-        } else {
-            let mut result = compiler.compile_expr(arg2)?;
-            result.append(&mut compiler.compile_expr(arg1)?);
-            result.push(Instruction::GtEq);
-            Ok(result)
-        }
-    })
+    compile_fn_compare(compiler, name, args, Instruction::GtEq)
 }
 
 pub(super) fn compile_fn_eq(
