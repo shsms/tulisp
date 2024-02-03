183bb277771f4c565ef57df0c842e521a469d08b
Sahas Subramanian <sahas.subramanian@proton.me>
2024-02-03 11:22:49 +0100

    Update `fround` to use new special form evaluator

--- src/builtin/functions/numbers/rounding_operations.rs
+++ src/builtin/functions/numbers/rounding_operations.rs
@@ -1,21 +1,14 @@
 use std::rc::Rc;
 
-use crate::{eval::eval_and_then, Error, ErrorKind, TulispContext, TulispObject, TulispValue};
+use crate::{
+    builtin::functions::common::eval_1_arg_special_form, eval::eval_and_then, Error, ErrorKind,
+    TulispContext, TulispObject, TulispValue,
+};
 
 pub(crate) fn add(ctx: &mut TulispContext) {
     fn fround(ctx: &mut TulispContext, args: &TulispObject) -> Result<TulispObject, Error> {
-        match args.cdr_and_then(|x| Ok(x.null())) {
-            Err(err) => return Err(err),
-            Ok(false) => {
-                return Err(Error::new(
-                    ErrorKind::TypeMismatch,
-                    format!("Expected exatly 1 argument for fround. Got args: {}", args),
-                ));
-            }
-            Ok(true) => {}
-        }
-        args.car_and_then(|arg| {
-            eval_and_then(ctx, &arg, |x| {
+        eval_1_arg_special_form(ctx, "fround", args, false, |ctx, arg1, _| {
+            eval_and_then(ctx, arg1, |x| {
                 if x.floatp() {
                     Ok(f64::round(x.as_float().unwrap()).into())
                 } else {
