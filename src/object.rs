b09de69074175f915d5648f4cbfb9f1a32998a0b
Sahas Subramanian <sahas.subramanian@proton.me>
2025-10-04 14:31:32 +0200

    Add an `Arc<RwLock<...>>` implementation of `TulispObject`
    It can be enabled with the `sync_send` feature.

--- src/object.rs
+++ src/object.rs
@@ -1,3 +1,11 @@
+#[cfg(not(feature = "sync_send"))]
 mod rc_refcell;
 
+#[cfg(not(feature = "sync_send"))]
 pub use rc_refcell::{Span, TulispObject};
+
+#[cfg(feature = "sync_send")]
+mod arc_rwlock;
+
+#[cfg(feature = "sync_send")]
+pub use arc_rwlock::{Span, TulispObject};
