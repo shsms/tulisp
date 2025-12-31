cec75c9537a1bfaac9b93769f7112e8e0daee9c6
Sahas Subramanian <sahas.subramanian@proton.me>
2025-12-31 16:53:21 +0100

    Simplify rounding implementations

--- src/builtin/functions/numbers/rounding_operations.rs
+++ src/builtin/functions/numbers/rounding_operations.rs
@@ -1,29 +1,15 @@
-use crate::{Error, TulispContext, destruct_eval_bind};
+use crate::{TulispContext, destruct_eval_bind};
 
 pub(crate) fn add(ctx: &mut TulispContext) {
     ctx.add_special_form("fround", |ctx, args| {
         destruct_eval_bind!(ctx, (x) = args);
 
-        if x.floatp() {
-            Ok(f64::round(x.as_float().unwrap()).into())
-        } else {
-            Err(Error::type_mismatch(format!(
-                "Expected float for fround. Got: {}",
-                x,
-            )))
-        }
+        Ok(f64::round(x.as_float()?).into())
     });
 
     ctx.add_special_form("ftruncate", |ctx, args| {
         destruct_eval_bind!(ctx, (x) = args);
 
-        if x.floatp() {
-            Ok(f64::trunc(x.as_float().unwrap()).into())
-        } else {
-            Err(Error::type_mismatch(format!(
-                "Expected float for ftruncate. Got: {}",
-                x,
-            )))
-        }
+        Ok(f64::trunc(x.as_float()?).into())
     });
 }
