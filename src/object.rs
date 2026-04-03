91b7b99cbd2740cc0542784ee25435c7144c3ab7
Sahas Subramanian <sahas.subramanian@proton.me>
2026-04-03 22:57:32 +0200

    Impl TryFrom<TulispObject> for bool instead of From<TulispObject>
    This makes bool similar to all other types.

--- src/object.rs
+++ src/object.rs
@@ -556,15 +556,19 @@ impl TryFrom<&TulispObject> for String {
     }
 }
 
-impl From<TulispObject> for bool {
-    fn from(value: TulispObject) -> Self {
-        value.is_truthy()
+impl TryFrom<TulispObject> for bool {
+    type Error = Error;
+
+    fn try_from(value: TulispObject) -> Result<Self, Self::Error> {
+        Ok(value.is_truthy())
     }
 }
 
-impl From<&TulispObject> for bool {
-    fn from(value: &TulispObject) -> Self {
-        value.is_truthy()
+impl TryFrom<&TulispObject> for bool {
+    type Error = Error;
+
+    fn try_from(value: &TulispObject) -> Result<Self, Self::Error> {
+        Ok(value.is_truthy())
     }
 }
 
